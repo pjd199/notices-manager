ab74d4f092db9d4e4a4db2f306ddcbf62fe32465
pjd199 <89210268+pjd199@users.noreply.github.com>
2026-05-13 08:10:32 +0000

    bump

--- advanced-notices-manager.php
+++ advanced-notices-manager.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Advanced Notices Manager
  * Description: Notice manager designed for Horsham Churches Together
- * Version: 2.1.10
+ * Version: 2.1.11
  * Author: Pete Dibdin
  * License: MIT
  * Plugin URI: https://github.com/pjd199/notices-manager
