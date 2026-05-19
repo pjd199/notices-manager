3f4da73ab57ee5acd5e6b075b50bf63636d3a67c
pjd199 <89210268+pjd199@users.noreply.github.com>
2026-05-19 19:54:40 +0000

    bug fix word count

--- src/word-count/index.js
+++ src/word-count/index.js
@@ -16,7 +16,7 @@ function updateExcerptCount() {
     }
 
     const text = excerptField.value.trim();
-    const wordCount = text.match(/\b[-?(\w+)]+/gi).length;
+    const wordCount = text?.match(/\b[-?(\w+)]+/gi)?.length ?? 0;
     
     const { excerptMin, excerptMax } = window.ANM_SETTINGS;
     const isWithinRange = wordCount >= excerptMin && wordCount <= excerptMax;
@@ -50,47 +50,3 @@ document.addEventListener('input', (e) => {
         updateExcerptCount();
     }
 });
-
-/*
-let prevValue = '';
-
-
-subscribe(() => {
-    console.log("subscribe");
-    console.log(window.ANM_SETTINGS);
-
-    if (!window.ANM_SETTINGS) {
-        return;
-    }
-
-    const { excerptMin, excerptMax } = window.ANM_SETTINGS;
-    const excerpt = select('core/editor').getEditedPostAttribute('excerpt');
-    
-    if (excerpt !== prevValue) {
-        prevValue = excerpt;
-
-        console.log(`Current Count: ${excerpt?.split(/\s+/).length || 0}`);
-
-        const excerptField = document.querySelector('.editor-post-excerpt textarea');
-        if (!excerptField) return;
-
-        let counter = document.getElementById('custom-excerpt-counter');
-        if (!counter) {
-            counter = document.createElement('div');
-            counter.id = 'custom-excerpt-counter';
-            counter.style = 'text-align: right; font-size: 12px; font-weight: bold; margin-top: 5px;';
-            excerptField.parentNode.insertBefore(counter, excerptField.nextSibling);
-        }
-
-        const text = excerptField.value.trim();
-        const wordCount = text ? text.split(/\s+/).length : 0;
-        
-        let statusColor = (wordCount >= excerptMin && wordCount <= excerptMax) 
-                            ? '#46b450'
-                            : (wordCount > excerptMax ? '#dc3232' : '#757575');
-
-        counter.style.color = statusColor;
-        counter.innerHTML = `Words: ${wordCount} ${wordCount >= excerptMin && wordCount <= excerptMax ? '✓' : ''}`;
-    }
-});
-*/
